c0bc4dce50fba4cdc99063ffe05cfc7be00d0257
Nasim Uddin <64758014+Nasim25@users.noreply.github.com>
2026-01-15 10:46:59 +0600

    Update login component option api to composition api
    Update login component option api to composition api

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -2,7 +2,8 @@
     <div
         class="max-w-md mx-auto mt-20 p-6 rounded-2xl shadow hover:-translate-y-1 ease-in duration-200 card text-left border-l-4 border-l-emerald-500">
         <h1 class="text-2xl font-bold mb-4 text-white">{{ $t('login') }}</h1>
-        <input v-model="email" type="email" :placeholder="$t('email')" class="w-full p-2 border mb-3 rounded text-white" />
+        <input v-model="email" type="email" :placeholder="$t('email')"
+            class="w-full p-2 border mb-3 rounded text-white" />
         <input v-model="password" type="password" :placeholder="$t('password')"
             class="w-full p-2 border mb-3 rounded text-white" />
         <button @click="login" class="bg-green-500 text-white w-full py-2 rounded cursor-pointer">
@@ -16,27 +17,35 @@
     </div>
 </template>
 
-<script>
-export default {
-    data() {
-        return { email: "", password: "", message: "" };
-    },
-    methods: {
-        async login() {
-            try {
-                const res = await this.$axios.post("/api/login", {
-                    email: this.email,
-                    password: this.password,
-                });
-                localStorage.setItem("token", res.data.access_token);
-                this.message = "Login successful!";
-                window.location.href = "/dashboard";
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+const message = ref("");
 
-            } catch (err) {
-                this.message =
-                    err.response?.data?.message || "Invalid credentials";
-            }
-        },
-    },
+const login = async () => {
+    try {
+        const res = await axios.post("/api/login", {
+            email: email.value,
+            password: password.value,
+        });
+
+        localStorage.setItem("token", res.data.access_token);
+        message.value = "Login successful!";
+        window.location.href = "/dashboard";
+    } catch (err) {
+        message.value =
+            err.response?.data?.message || "Invalid credentials";
+    }
 };
+
+onMounted(async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        router.push("/dashboard");
+    }
+});
 </script>
